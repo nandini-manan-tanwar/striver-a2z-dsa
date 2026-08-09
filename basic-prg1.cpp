2bba6e13efb03f04fcdf8a568b8427ccea1bea5e
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-09 19:37:31 +0530

    add input_output-prg1

--- basic-prg1.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"hi there,nandini here"<<"\n";
-    cout<<"hi there,nandini here";
-
-};
---
+++ input_output-prg1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"hi there,nandini here"<<"\n";
+    cout<<"hi there,nandini here"<<"\n";
+    int x,y;
+    cin >> x>>y;
+    cout << "value of x is ="<< x<<"\n";
+    cout << "value of y is ="<< y;
+
+};
