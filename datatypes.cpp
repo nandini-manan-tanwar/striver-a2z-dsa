3b0a837f549d074f0d147bf36acfda6221d3818f
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-09 21:43:03 +0530

    add datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,13 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  // int ,long,long long,float,double
+  // string,getline 
+  // char (in single quotes)(stores everything)
+  string str;
+  cout<<"input is:";
+  getline(cin,str);
+  cout<<str;
+
+};
