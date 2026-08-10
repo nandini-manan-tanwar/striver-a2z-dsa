674471242a6bf1f96c04bce5721500f000eedd04
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-10 21:33:04 +0530

    add loop+array

---
+++ loop+array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,arr[5];
+    for(i=0;i<=4;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"the array is "<<endl;
+    for(i=0;i<=4;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
