9e02121a11d2958638e50059fef189972a6ec3dc
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-12 19:45:12 +0530

    add pattern4

---
+++ pattern4.cpp
@@ -0,0 +1,22 @@
+/*
+1
+22
+333
+4444
+55555
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i=0;i<=5;i++)
+    {
+        for (int j=0;j<i;j++)
+      {
+        cout << i;
+      }
+      cout<<"\n";
+    }
+
+
+}
