31eceb6d0f7b5d61a7d7604b4dbd78a8b536e38c
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-12 19:51:38 +0530

    add pattern5

---
+++ pattern5.cpp
@@ -0,0 +1,23 @@
+/*
+* * * * *
+* * * * 
+* * * 
+* * 
+*
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i=5;i>=1;i--)
+    {
+        for (int j=0;j<i;j++)
+      {
+        cout << " "<<"*";
+      }
+      cout<<"\n";
+    }
+
+
+}
