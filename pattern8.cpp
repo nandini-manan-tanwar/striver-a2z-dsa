e057601649228cedd763935a4f3c4dd79888de73
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-13 21:07:39 +0530

    add pattern8

---
+++ pattern8.cpp
@@ -0,0 +1,24 @@
+/*
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i=5;i>=0;i--)
+    {
+        for (int space=4;space>=i;space--)
+      {
+        cout << " ";
+      }
+      
+        for (int j=0;j<2*i+1;j++)
+      {
+        cout << "*";
+      }
+
+      cout<<"\n";
+    }
+
+
+}
