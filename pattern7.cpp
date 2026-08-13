fcd8d3253e6930a24e161181b19f8e6a4770082f
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-13 19:50:02 +0530

    add pattern7

---
+++ pattern7.cpp
@@ -0,0 +1,29 @@
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+  
+  */
+ #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i=0;i<5;i++)
+    {
+        for (int space=5;space>i;space--)
+      {
+        cout << " ";
+      }
+      
+        for (int j=0;j<2*i+1;j++)
+      {
+        cout << "*";
+      }
+
+      cout<<"\n";
+    }
+
+
+}
