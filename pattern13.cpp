ba52d8b3f223ceaac82b1f44f2c6341a1877d8d4
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-14 21:55:32 +0530

    update pattern13

--- pattern13.cpp
+++ pattern13.cpp
@@ -12,12 +12,14 @@
 using namespace std;
 int main()
 {
+    int cnt=1;
     for (int i=1;i<=5;i++)
     {
      for (int j=1;j<=i;j++)
       {
-        cout <<i+1;
-        
+        cout <<cnt;
+        cnt++;
+        cout<<" ";
 
       }
 
