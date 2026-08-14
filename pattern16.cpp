3648fa44ad7df54890c6ad64b8af41a9ce3de267
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-14 22:14:41 +0530

    add pattern16

---
+++ pattern16.cpp
@@ -0,0 +1,22 @@
+/*
+ A
+ B B
+ C C C
+ D D D D
+ E E E E E
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (char i='A';i<='E';i++)
+    {
+        for (char j='A';j<=i;j++)
+      {
+        cout << " "<<i;
+      }
+      cout<<"\n";
+    }
+
+
+}
