2ccd5e490dc17c9698dc47288a5c28106750f415
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-16 21:27:09 +0530

    add pattern20

---
+++ pattern20.cpp
@@ -0,0 +1,56 @@
+/*
+*        *
+**      **
+***    ***
+****  ****
+**********
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int cnt1=0;
+for (int i = 1; i <=5; i++)
+{
+    for (int j = 1; j <=i; j++)
+    {
+       cout <<"*";
+    }
+     for (int space2=8;space2>cnt1;space2--)
+      {
+        cout << " ";
+      }
+    cnt1=cnt1+2;
+    for (int o = 1; o<=i; o++)
+    {
+       cout <<"*";
+    }
+    
+    cout<<"\n";
+}
+int cnt=0;
+    for (int i=5;i>=1;i--)
+    {
+        for (int j=0;j<i;j++)
+      {
+        cout <<"*";
+      }
+       for (int space1=0;space1<cnt*2;space1++)
+      {
+        cout << " ";
+      }
+      cnt++;
+       for (int k=0;k<i;k++)
+      {
+        cout <<"*";
+      }
+ 
+      cout<<"\n";
+    }
+
+}
