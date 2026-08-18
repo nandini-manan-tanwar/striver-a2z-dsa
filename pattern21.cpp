62eb46c3ea3e618d4f5400154df4af8ee26eed51
Nandini <nandinimanantanwar.csai29@jecrc.ac.in>
2026-08-18 19:30:10 +0530

    add pattern21

---
+++ pattern21.cpp
@@ -0,0 +1,39 @@
+/*
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i=1;i<=8;i++)
+    {
+       if (i==1||i==3||i==6||i==8)
+       {
+          if (i==1||i==8)
+          { for (int j = 1; j <=4; j++)
+              {
+                 cout <<"*"<<" ";
+              }
+          }
+          else 
+          {
+            cout<<"*";
+            for (int j = 1; j <=4; j++)
+              {
+                 cout <<" ";
+              }
+            cout <<" "<<"*";
+          }
+        }
+        else
+        {
+            for (int j = 1; j <=4; j++)
+              {
+                 cout <<" ";
+              }
+
+        }
+      cout<<"\n";
+    }
+
+
+}
